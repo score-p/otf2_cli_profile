e6ec41f2485a2c42a431ce14db3439549e0fa2fb
Loc Nguyen Dang Duc <loc.nguyen_dang_duc@tu-dresden.de>
2018-08-08 16:58:33 +0200

    correct include and exclude time, add timerresolution

--- src/output/create_dot.cpp
+++ src/output/create_dot.cpp
@@ -5,56 +5,61 @@
 
 #include "create_dot.h"
 
-struct Date {
+struct Node {
 	uint32_t call_id = 0;
+	
 	std::string region;
 	uint32_t invocations = 0;
 	uint32_t num_children = 0;
+	
 	double min_incl_time = std::numeric_limits<uint64_t>::max();
-	double min_excl_time = std::numeric_limits<uint64_t>::max();
 	double max_incl_time = 0;
-	double max_excl_time = 0;
 	double sum_incl_time = 0;
+
+	double min_excl_time = std::numeric_limits<uint64_t>::max();
+	double max_excl_time = 0;
 	double sum_excl_time = 0;
 };
 
-typedef std::vector<Date> Data;
-Data data;
+typedef std::vector<Node> Data;
+
+
 
 Data read_data(AllData& alldata){
 	
-	Date date;
+	Data data;
 	
 	for (auto& region : alldata.call_path_tree) {
+		Node node;
 
-		std::string func_name;
-		func_name = alldata.definitions.regions.get(region.function_id)->name;
+		std::string region_name = alldata.definitions.regions.get(region.function_id)->name;
 		
-		date.region = func_name;
-		++date.call_id; 
-		date.num_children = region.children.size();
+		node.region = region_name;
 		
+		node.num_children = region.children.size();
+
+		double timerResolution = (double)alldata.metaData.timerResolution;
 		// acumulate data over all locations
 		for (auto& location : region.node_data) {	
 
-			date.invocations += location.second.f_data.count;
+			node.invocations += location.second.f_data.count;
 
-			double incl_time = location.second.f_data.incl_time;
-			if (date.min_incl_time > incl_time)
-				date.min_incl_time = incl_time;
-			if (date.max_incl_time < incl_time)
-				date.max_incl_time = incl_time;
-			date.sum_incl_time += incl_time;
+			double incl_time = location.second.f_data.incl_time / timerResolution;
+			if (node.min_incl_time > incl_time)
+				node.min_incl_time = incl_time;
+			if (node.max_incl_time < incl_time)
+				node.max_incl_time = incl_time;
+			node.sum_incl_time += incl_time;
 
-			double excl_time = location.second.f_data.excl_time;
-			if (date.min_excl_time > excl_time)
-				date.min_excl_time = excl_time;
-			if (date.max_excl_time < excl_time)
-				date.max_excl_time = excl_time;
-			date.sum_excl_time += excl_time;			
+			double excl_time = location.second.f_data.excl_time / timerResolution;
+			if (node.min_excl_time > excl_time)
+				node.min_excl_time = excl_time;
+			if (node.max_excl_time < excl_time)
+				node.max_excl_time = excl_time;
+			node.sum_excl_time += excl_time;			
 		}
 
-		data.push_back(date);
+		data.push_back(node);
 	}
 	return data;
 
@@ -63,6 +68,7 @@ Data read_data(AllData& alldata){
 void write_dot(Data data) {
 
 	std::stack<uint32_t> tmp;
+	int call_id = 0;
 
 	std::ofstream result_file;
 	result_file.open ("result.dot");
@@ -75,7 +81,6 @@ void write_dot(Data data) {
 	<< std::endl;
 
 	for ( auto& region : data ){
-		uint32_t call_id = region.call_id;
 		result_file 
 			<< "\"" << call_id << "\" [\n"
 			<< "label = \"" 
@@ -109,6 +114,8 @@ void write_dot(Data data) {
 		// trick to remember parents
 		for (int i = 0; i < region.num_children; ++i)
 			tmp.push(call_id);
+
+		++call_id; 
 	}
 
 	result_file << "}" << std::endl;
