2cd3bb1a2cfde16411cb22525415face2589b87f
NFues <nico.fuessel@tu-dresden.de>
2022-08-17 10:46:09 +0200

    Key Format from snake_case to camelCase

--- src/output/data_out.cpp
+++ src/output/data_out.cpp
@@ -44,9 +44,9 @@ void display(Writer& writer, AllData alldata){
 
 template <typename Writer>
 void display_data_tree(AllData alldata, Writer& writer){
-    writer.String("call_tree");
+    writer.String("callTree");
     writer.StartObject();
-        writer.Key("root_nodes");
+        writer.Key("rootNodes");
         writer.StartArray();
             for(const auto& node : alldata.call_path_tree.root_nodes){
                 display_node(node.second, writer);
@@ -58,7 +58,7 @@ void display_data_tree(AllData alldata, Writer& writer){
 template <typename Writer>
 void display_node(std::shared_ptr<tree_node> node, Writer& writer){
     writer.StartObject();
-        writer.Key("region_id");
+        writer.Key("regionId");
         writer.Uint64(node->function_id);
 
         writer.Key("parent");
@@ -71,37 +71,37 @@ void display_node(std::shared_ptr<tree_node> node, Writer& writer){
         writer.StartArray();
             for(const auto& data : node->node_data){
                 writer.StartObject();
-                    writer.Key("location_id");
+                    writer.Key("locationId");
                     writer.Uint64(data.first);
-                    writer.Key("f_data");
+                    writer.Key("fData");
                     writer.StartObject();
                         writer.Key("count");
                         writer.Uint64(data.second.f_data.count);
-                        writer.Key("incl_time");
+                        writer.Key("inclTime");
                         writer.Uint64(data.second.f_data.incl_time);
-                        writer.Key("excl_time");
+                        writer.Key("exclTime");
                         writer.Uint64(data.second.f_data.excl_time);
                     writer.EndObject();
-                    writer.Key("m_data");
+                    writer.Key("mData");
                     writer.StartObject();
-                        writer.Key("count_send");
+                        writer.Key("countSend");
                         writer.Uint64(data.second.m_data.count_send);
-                        writer.Key("count_recv");
+                        writer.Key("countRecv");
                         writer.Uint64(data.second.m_data.count_recv);
-                        writer.Key("bytes_send");
+                        writer.Key("bytesSend");
                         writer.Uint64(data.second.m_data.bytes_send);
-                        writer.Key("bytes_recv");
+                        writer.Key("bytesRecv");
                         writer.Uint64(data.second.m_data.bytes_recv);
                     writer.EndObject();
-                    writer.Key("c_data");
+                    writer.Key("cData");
                     writer.StartObject();
-                        writer.Key("count_send");
+                        writer.Key("countSend");
                         writer.Uint64(data.second.c_data.count_send);
-                        writer.Key("count_recv");
+                        writer.Key("countRecv");
                         writer.Uint64(data.second.c_data.count_recv);
-                        writer.Key("bytes_send");
+                        writer.Key("bytesSend");
                         writer.Uint64(data.second.c_data.bytes_send);
-                        writer.Key("bytes_recv");
+                        writer.Key("bytesRecv");
                         writer.Uint64(data.second.c_data.bytes_recv);
                     writer.EndObject();
                     writer.Key("metrics");
@@ -110,9 +110,9 @@ void display_node(std::shared_ptr<tree_node> node, Writer& writer){
                             writer.StartObject();
                                 writer.Key(std::to_string(metricData.first).c_str());
                                 writer.StartObject();
-                                    writer.Key("MetricDataType");
+                                    writer.Key("metricDataType");
                                     writer.Uint(static_cast<uint8_t>(metricData.second.type));
-                                    writer.Key("data_incl");
+                                    writer.Key("dataIncl");
                                     writer.StartObject();
                                         writer.Key("u");
                                         writer.Uint64(metricData.second.data_incl.u);
@@ -121,7 +121,7 @@ void display_node(std::shared_ptr<tree_node> node, Writer& writer){
                                         writer.Key("d");
                                         writer.Double(metricData.second.data_incl.d);
                                     writer.EndObject();
-                                    writer.Key("data_excl");
+                                    writer.Key("dataExcl");
                                     writer.StartObject();
                                         writer.Key("u");
                                         writer.Uint64(metricData.second.data_excl.u);
@@ -145,10 +145,10 @@ void display_node(std::shared_ptr<tree_node> node, Writer& writer){
             }
         writer.EndArray();
 
-        writer.Key("has_p2p");
+        writer.Key("hasP2p");
         writer.Bool(node->has_p2p);
 
-        writer.Key("has_collop");
+        writer.Key("hasCollop");
         writer.Bool(node->has_collop);
 
         writer.EndObject();
@@ -156,7 +156,7 @@ void display_node(std::shared_ptr<tree_node> node, Writer& writer){
 
 template <typename Writer>
 void display_definitions(AllData alldata, Writer& writer){
-    writer.Key("Definitions");
+    writer.Key("definitions");
     writer.StartObject();
         writer.Key("paradigms");
         writer.StartArray();
@@ -172,15 +172,15 @@ void display_definitions(AllData alldata, Writer& writer){
         writer.StartArray();
             for(const auto& region : alldata.definitions.regions.get_all()){
                 writer.StartObject();
-                    writer.Key("region_id");
+                    writer.Key("regionId");
                     writer.Uint(region.first);
                     writer.Key("name");
                     writer.String(region.second.name.c_str());
-                    writer.Key("paradigm_id");
+                    writer.Key("paradigmId");
                     writer.Uint(region.second.paradigm_id);
-                    writer.Key("source_line");
+                    writer.Key("sourceLine");
                     writer.Uint(region.second.source_line);
-                    writer.Key("file_name");
+                    writer.Key("fileName");
                     writer.String(region.second.file_name.c_str());
                 writer.EndObject();
             }
@@ -191,7 +191,7 @@ void display_definitions(AllData alldata, Writer& writer){
             for(const auto& it : alldata.definitions.metrics.get_all()){
                 const definitions::Metric& metric = it.second;
                 writer.StartObject();
-                        writer.Key("metric_id");
+                        writer.Key("metricId");
                         writer.Uint64(it.first);
                         writer.Key("name");
                         writer.String(metric.name.c_str());
@@ -215,17 +215,17 @@ void display_definitions(AllData alldata, Writer& writer){
             }
         writer.EndArray();
 
-        writer.Key("metric_classes");
+        writer.Key("metricClasses");
         writer.StartArray();
             for(const auto& it : alldata.definitions.metric_classes.get_all()){
                 const definitions::Metric_Class& metric_class = it.second;
 
                 writer.StartObject();
-                        writer.Key("metric_class_id");
+                        writer.Key("metricClassId");
                         writer.Uint64(it.first);
-                        writer.Key("num_of_metrics");
+                        writer.Key("numOfMetrics");
                         writer.Uint(metric_class.num_of_metrics);
-                        writer.Key("metric_member");
+                        writer.Key("metricMember");
                         writer.StartArray();
                             for(const auto& member : metric_class.metric_member){
                                 writer.StartObject();
@@ -234,15 +234,15 @@ void display_definitions(AllData alldata, Writer& writer){
                                 writer.EndObject();
                             }
                         writer.EndArray();
-                        writer.Key("metric_occurrence");
+                        writer.Key("metricOccurrence");
                         writer.Uint(static_cast<uint> (metric_class.metric_occurrence));
-                        writer.Key("recorder_kind");
+                        writer.Key("recorderKind");
                         writer.Uint(static_cast<uint> (metric_class.recorder_kind));
                 writer.EndObject();
             }
         writer.EndArray();
 
-        writer.Key("io_paradigms");
+        writer.Key("ioParadigms");
         writer.StartArray();
             for(const auto& io_paradigm : alldata.definitions.io_paradigms.get_all()){
                 writer.StartObject();
@@ -260,7 +260,7 @@ void display_definitions(AllData alldata, Writer& writer){
                     writer.StartObject();
                         writer.Key("name");
                         writer.String(iohandle.second.name.c_str());
-                        writer.Key("io_paradigm");
+                        writer.Key("ioParadigm");
                         writer.Uint(iohandle.second.io_paradigm);
                         writer.Key("file");
                         writer.Uint64(iohandle.second.file);
@@ -276,13 +276,13 @@ void display_definitions(AllData alldata, Writer& writer){
             for(const auto& it : alldata.definitions.groups.get_all()){
                 const definitions::Group& group = it.second;
                 writer.StartObject();
-                    writer.Key("group_id");
+                    writer.Key("groupId");
                     writer.Uint64(it.first);
                     writer.Key("name");
                     writer.String(group.name.c_str());
                     writer.Key("type");
                     writer.Uint(group.type);
-                    writer.Key("paradigm_id");
+                    writer.Key("paradigmId");
                     writer.Uint(group.paradigm_id);
                     writer.Key("members");
                     writer.StartArray();
@@ -299,7 +299,7 @@ void display_definitions(AllData alldata, Writer& writer){
 
 template <typename Writer>
 void display_meta_data(AllData alldata, Writer& writer){
-    writer.Key("meta_data");
+    writer.Key("metaData");
     writer.StartObject();
         writer.Key("timerResolution");
         writer.Double(alldata.metaData.timerResolution);
@@ -307,7 +307,7 @@ void display_meta_data(AllData alldata, Writer& writer){
         writer.Key("numRanks");
         writer.Uint64(alldata.metaData.numRanks);
 
-        writer.Key("input_file_name");
+        writer.Key("inputFileName");
         writer.String(alldata.params.input_file_name.c_str());
         
     writer.EndObject();
@@ -315,7 +315,7 @@ void display_meta_data(AllData alldata, Writer& writer){
 
 template <typename Writer>
 void display_meta_data_profiler(AllData alldata, Writer& writer){
-    writer.Key("meta_data_profiler");
+    writer.Key("metaDataProfiler");
     writer.StartObject();
     writer.Key("communicators");
         writer.StartArray();
@@ -382,42 +382,42 @@ template <typename Writer>
 void display_params(AllData alldata, Writer& writer){
     writer.Key("Params");
     writer.StartObject();
-        writer.Key("max_file_handles");
+        writer.Key("maxFileHandles");
         writer.Uint(alldata.params.max_file_handles);
-        writer.Key("buffer_size");
+        writer.Key("bufferSize");
         writer.Uint(alldata.params.buffer_size);
-        writer.Key("verbose_level");
+        writer.Key("verboseLevel");
         writer.Uint(alldata.params.verbose_level);
-        writer.Key("read_metrics");
+        writer.Key("readMetrics");
         writer.Bool(alldata.params.read_metrics);
-        writer.Key("output_type_set");
+        writer.Key("outputTypeSet");
         writer.Bool(alldata.params.output_type_set);
-        writer.Key("create_cube");
+        writer.Key("createCube");
         writer.Bool(alldata.params.create_cube);
-        writer.Key("create_json");
+        writer.Key("createJson");
         writer.Bool(alldata.params.create_json);
-        writer.Key("data_dump");
+        writer.Key("dataDump");
         writer.Bool(alldata.params.data_dump);
-        writer.Key("summarize_it");
+        writer.Key("summarizeIt");
         writer.Bool(alldata.params.summarize_it);
-        writer.Key("input_file_name");
+        writer.Key("inputFileName");
         writer.String(alldata.params.input_file_name.c_str());
-        writer.Key("input_file_prefix");
+        writer.Key("inputFilePrefix");
         writer.String(alldata.params.input_file_prefix.c_str());
-        writer.Key("output_file_prefix");
+        writer.Key("outputFilePrefix");
         writer.String(alldata.params.output_file_prefix.c_str());
     writer.EndObject();
 }
 
 template <typename Writer>
 void display_system_tree(AllData alldata, Writer& writer){
-    writer.Key("system_tree");
+    writer.Key("systemTree");
     writer.StartObject();
-        writer.Key("system_nodes");
+        writer.Key("systemNodes");
         display_system_node(alldata.definitions.system_tree.get_root(), alldata, writer);
         writer.Key("_size");
         writer.Uint(alldata.definitions.system_tree.size());
-        writer.Key("num_nodes_per_level");
+        writer.Key("numNodesPerLevel");
         writer.StartArray();
             for(const auto& num : alldata.definitions.system_tree.all_level())
                 writer.Uint(num);
@@ -439,13 +439,13 @@ void display_system_node(std::shared_ptr<definitions::SystemTree::SystemNode> no
 
         writer.Key("data");
         writer.StartObject();
-            writer.Key("node_id");
+            writer.Key("nodeId");
             writer.Uint(node->data.node_id);
             writer.Key("name");
             writer.String(node->data.name.c_str());
-            writer.Key("class_id");
+            writer.Key("classId");
             writer.Uint(static_cast<uint8_t> (node->data.class_id));
-            writer.Key("location_id");
+            writer.Key("locationId");
             writer.Uint64(node->data.location_id);
             writer.Key("level");
             writer.Uint(node->data.level);
